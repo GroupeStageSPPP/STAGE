8cb335514334b11bd1d75e421fecf4d2587e5d9c
JB <jb.blois@iia-laval.fr>
2014-05-23 12:01:13 +0200

    Ajout du Qualite.DB (corectif du commit anterieur)

--- QualiteSPPP/QualiteSPPP.DB/Properties/AssemblyInfo.cs
+++ QualiteSPPP/QualiteSPPP.DB/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("9c6de2c3-4c48-4339-96a6-550c3bc22850")]
+[assembly: Guid("396b6f35-2bb9-45d5-b08d-aecc25460b1e")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
